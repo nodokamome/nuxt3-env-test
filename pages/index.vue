6f822ae79663de86a62c25225dfcd858cd2518f1
nodokamome <mail@nodokamome.com>
2022-12-28 15:02:01 +0900

    set env test

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+console.log(config.public.apiBase)
+</script>
+
+<template>
+  <div>
+    <p>{{ config.public.apiBase }}</p>
+  </div>
+</template>
